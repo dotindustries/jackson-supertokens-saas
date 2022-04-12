7e2adfeaaefbf5520c8722b62ae2810e876cd890
János Veres <janos@dot.industries>
2022-04-12 20:31:38 +0200

    todos for after using supertokens' provider helper methods

--- packages/app-config/src/auth/frontendConfig.ts
+++ packages/app-config/src/auth/frontendConfig.ts
@@ -11,13 +11,17 @@ export const frontendConfig = () => ({
         let action = context.action
 
         if (action === 'GET_AUTHORISATION_URL') {
+          // TODO check if we already have a tenant and do not overwrite
           url.searchParams.append('tenant', 'boxyhq.com')
+          // TODO product should only be set here
           url.searchParams.append('product', 'supertokens')
           console.log('get_authorisation_url preAPIHook called and appended params', url)
         }
 
         if (action === 'THIRD_PARTY_SIGN_IN_UP') {
+          // TODO check if we already have a tenant and do not overwrite
           url.searchParams.append('tenant', 'boxyhq.com')
+          // TODO product should only be set here
           url.searchParams.append('product', 'supertokens')
           console.log('third_party_sign_in_up preAPIHook called and appended params', url)
         }
